9610aa1734d8aad4ae47a9d40a0974619d90efe6
19nikilangiu91 <niki.langiu@gmail.com>
2022-11-04 14:08:29 +0100

    Upgrade Comments

--- js/main.js
+++ js/main.js
@@ -1,2 +1,27 @@
 // DEBUG
 // alert('Hello World')
+
+
+
+// Variabili
+
+
+// Chiedere all'utente il suo Nome.
+
+
+// Chiedere all'utente il suo Cognome.
+
+
+// Chiedere all'utente il suo Colore preferito.
+
+
+// Inserire la costante con il numero 21 alla fine.
+
+
+// Output- sulla pagina dovrà apparire nomecognomecolorepreferito21.
+
+
+
+
+
+
